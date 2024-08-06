fdc36c4178a053712b8fbecb68a209aa0b5bf2cc
JotaDD <joaoeduardogomes@gmail.com>
2024-08-05 21:43:31 -0300

    chore: create migrations

--- internal/store/pgstore/migrations/001_create_rooms_table.sql
+++ internal/store/pgstore/migrations/001_create_rooms_table.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS rooms (
   "id"    uuid          PRIMARY KEY   NOT NULL   DEFAULT gen_random_uuid(),
-  "theme" VARCHAR(255)  PRIMARY KEY   NOT NULL
+  "theme" VARCHAR(255)                NOT NULL
 );
 
 ---- create above / drop below ----
